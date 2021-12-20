55e60b4e2f2c588c04a68e0dfd869295926c562d
anasseddaoudi <anass.eddaoudiz@hotmail.com>
2021-12-19 21:42:18 -0500

    test+case 6

--- src/vaxtodo/Menu.java
+++ src/vaxtodo/Menu.java
@@ -3,36 +3,36 @@ package vaxtodo;
 import java.util.Scanner;
 
 public class Menu {
-	
-	private User currentUser;
-	private LoginController login;
-	private ReservationController reservation;
-	private VisitorController visitor;
-	private UserController user;
-	
-	public Menu () {
-		login = new LoginController();
-		reservation = new ReservationController();
-		visitor = new VisitorController();
-		user = new UserController();
-	}
 
-	public void open() {
-		currentUser = login.login();
-		displayMenu();
-	}
+    private User currentUser;
+    private LoginController login;
+    private ReservationController reservation;
+    private VisitorController visitor;
+    private UserController user;
 
-	public User getCurrentUser() {
-		return currentUser;
-	}
-	
-	public void setCurrentUser(User user) {
-		currentUser = user;
-	}
+    public Menu() {
+        login = new LoginController();
+        reservation = new ReservationController();
+        visitor = new VisitorController();
+        user = new UserController();
+    }
 
-	public void displayMenu() {
-		
-		Scanner scanner = new Scanner(System.in);
+    public void open() {
+        currentUser = login.login();
+        displayMenu();
+    }
+
+    public User getCurrentUser() {
+        return currentUser;
+    }
+
+    public void setCurrentUser(User user) {
+        currentUser = user;
+    }
+
+    public void displayMenu() {
+
+        Scanner scanner = new Scanner(System.in);
         boolean isContinue = true;
         while (isContinue) {
             System.out.println("\n1. Changer le compte utilisateur");
@@ -40,7 +40,7 @@ public class Menu {
             System.out.println("3. Gestion de visiteurs");
             System.out.println("4. Gestion d'utilisateurs");
             System.out.println("5. Consultation d'informations");
-            System.out.println("6. ");
+            System.out.println("6. Rapport de vaccination");
             System.out.println("7. Quitter");
             System.out.print("Choisissez une option : ");
 
@@ -48,27 +48,27 @@ public class Menu {
 
             switch (choice) {
                 case 1:
-                	login.logout();
-                	break;
+                    login.logout();
+                    break;
                 case 2:
-                	displayReservation();
-                	break;
+                    displayReservation();
+                    break;
                 case 3:
-                	displayVisitor();
-                	break;
+                    displayVisitor();
+                    break;
                 case 4:
-                	displayUser();
-                	break;
+                    displayUser();
+                    break;
                 case 5:
-                	displayInfo();
-                	break;
+                    displayInfo();
+                    break;
                 case 6:
-                	
-                	break;
+                    getVacciationReport();
+                    break;
                 case 7:
-                	isContinue = false;
-                	System.out.println("Fin de session");
-                	break;
+                    isContinue = false;
+                    System.out.println("Fin de session");
+                    break;
                 default:
                     System.out.println("La valeur entree est invalide");
                     break;
@@ -76,11 +76,33 @@ public class Menu {
             }
 
         }
-		
-	}
 
-	public void displayReservation() {
-		Scanner scanner = new Scanner(System.in);
+    }
+
+    public void getVacciationReport() {
+        Scanner scanner = new Scanner(System.in);
+        boolean isContinue = true;
+        while (isContinue) {
+            System.out.println("\n1. Creer un rapport");
+            System.out.println("2. Envoyer le rapport");
+        }
+        int choice = scanner.nextInt();
+
+        switch (choice) {
+            case 1:
+                reservation.createReport();
+                break;
+            case 2:
+                reservation.notifyVisitors();
+                break;
+            default:
+                System.out.println("La valeur entree est invalide");
+        }
+
+    }
+
+    public void displayReservation() {
+        Scanner scanner = new Scanner(System.in);
         boolean isContinue = true;
         while (isContinue) {
             System.out.println("\n1. Creer une reservation");
@@ -93,24 +115,25 @@ public class Menu {
 
             switch (choice) {
                 case 1:
-                	reservation.createReservation();
-                	break;
+                    reservation.createReservation();
+                    break;
                 case 2:
-                	reservation.deleteReservation();
-                	break;
+                    reservation.deleteReservation();
+                    break;
                 case 3:
-                	reservation.confirmReservation();
-                	break;
+                    reservation.confirmReservation();
+                    break;
                 case 4:
-                	isContinue = false;
-                	break;
-                default : System.out.println("La valeur entree est invalide");
+                    isContinue = false;
+                    break;
+                default:
+                    System.out.println("La valeur entree est invalide");
             }
         }
-	}
+    }
 
-	public void displayVisitor() {
-		Scanner scanner = new Scanner(System.in);
+    public void displayVisitor() {
+        Scanner scanner = new Scanner(System.in);
         boolean isContinue = true;
         while (isContinue) {
             System.out.println("\n1. Creer un visiteur");
@@ -123,24 +146,25 @@ public class Menu {
 
             switch (choice) {
                 case 1:
-                	visitor.createVisitor();
-                	break;
+                    visitor.createVisitor();
+                    break;
                 case 2:
-                	visitor.deleteVisitor();
-                	break;
+                    visitor.deleteVisitor();
+                    break;
                 case 3:
-                	visitor.modifyVisitor();
-                	break;
+                    visitor.modifyVisitor();
+                    break;
                 case 4:
-                	isContinue = false;
-                	break;
-                default : System.out.println("La valeur entree est invalide");
+                    isContinue = false;
+                    break;
+                default:
+                    System.out.println("La valeur entree est invalide");
             }
         }
-	}
-	
-	public void displayUser() {
-		Scanner scanner = new Scanner(System.in);
+    }
+
+    public void displayUser() {
+        Scanner scanner = new Scanner(System.in);
         boolean isContinue = true;
         while (isContinue) {
             System.out.println("\n1. Creer un utilisateur");
@@ -153,27 +177,28 @@ public class Menu {
 
             switch (choice) {
                 case 1:
-                	user.createUser();
-                	break;
+                    user.createUser();
+                    break;
                 case 2:
-                	user.deleteUser();
-                	break;
+                    user.deleteUser();
+                    break;
                 case 3:
-                	user.modifyUser();
-                	break;
+                    user.modifyUser();
+                    break;
                 case 4:
-                	isContinue = false;
-                	break;
-                default : System.out.println("La valeur entree est invalide");
+                    isContinue = false;
+                    break;
+                default:
+                    System.out.println("La valeur entree est invalide");
             }
         }
-	}
-	
-	public void displayInfo() {
-		Scanner scanner = new Scanner(System.in);
+    }
+
+    public void displayInfo() {
+        Scanner scanner = new Scanner(System.in);
         boolean isContinue = true;
         while (isContinue) {
-            System.out.println("\n1. Consulter la liste des reservations");
+            System.out.println("\n1. Consulter le calendrier");
             System.out.println("2. Consulter la liste des visiteurs");
             System.out.println("3. Consulter la liste des utilisateurs");
             System.out.println("4. Quitter le menu de la consultation d'informations");
@@ -183,19 +208,20 @@ public class Menu {
 
             switch (choice) {
                 case 1:
-                	reservation.reservationList();
-                	break;
+
+                    break;
                 case 2:
                     visitor.visitorList();
-                	break;
+                    break;
                 case 3:
                     user.userList();
-                	break;
+                    break;
                 case 4:
-                	isContinue = false;
-                	break;
-                default : System.out.println("La valeur entree est invalide");
+                    isContinue = false;
+                    break;
+                default:
+                    System.out.println("La valeur entree est invalide");
             }
         }
-	}
+    }
 }
--- src/vaxtodo/ReservationController.java
+++ src/vaxtodo/ReservationController.java
@@ -1,42 +1,46 @@
 package vaxtodo;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 
 public class ReservationController {
 
 	public void createReservation() {
-		
+
 		String[] infos = new String[5];
 		Scanner scanner = new Scanner(System.in);
-		
+
 		System.out.println("\nCreation d'une nouvelle reservation");
-		
+
 		System.out.println("Entrer le nom de famille du visiteur : ");
 		infos[0] = scanner.nextLine();
-		
+
 		System.out.println("Entrer le prenom du visiteur : ");
 		infos[1] = scanner.nextLine();
-		
+
 		System.out.println("Entrer la date de la reservation (AAAA-MM-JJ) : ");
 		infos[2] = scanner.nextLine();
-		
+
 		System.out.println("Entrer l'heure de la reservation (HH:MM) : ");
 		infos[3] = scanner.nextLine();
-		
+
 		System.out.println("Numero de dose du vaccin : ");
 		infos[4] = scanner.nextLine();
-		
-		if(Reservation.isValid(infos)) {
-			VaxTodo.reservations.add(new Reservation(infos[0], infos[1], infos[2], infos[3], Integer.parseInt(infos[4])));
+
+		if (Reservation.isValid(infos)) {
+			VaxTodo.reservations
+					.add(new Reservation(infos[0], infos[1], infos[2], infos[3], Integer.parseInt(infos[4])));
 			System.out.println(Arrays.toString(VaxTodo.reservations.toArray()));
 			System.out.println("Reservation creee");
 			return;
 		}
-		
+
 		System.out.println("Voulez vous reessayer de creer une reservation? (OUI/NON)");
 		String decision = scanner.nextLine();
-		
-		if(decision.equals("OUI")) {
+
+		if (decision.equals("OUI")) {
 			createReservation();
 		}
 		return;
@@ -44,31 +48,31 @@ public class ReservationController {
 
 	public void deleteReservation() {
 		Scanner scanner = new Scanner(System.in);
-		
+
 		System.out.println("\nEntrer le numero de reservation de la reservation a supprimer (6 chiffres) : ");
 		String temp = scanner.nextLine();
 		int number = 0;
-		
+
 		try {
 			number = Integer.parseInt(temp);
-		}catch (NumberFormatException e){
+		} catch (NumberFormatException e) {
 			System.out.println("Numero de reservation invalide");
 			return;
 		}
-		
-		for(Reservation reservation : VaxTodo.reservations) {
-			if(reservation.getReservationNumber() == number) {
+
+		for (Reservation reservation : VaxTodo.reservations) {
+			if (reservation.getReservationNumber() == number) {
 				VaxTodo.reservations.remove(reservation);
 				System.out.println("La reservation a ete supprimee");
 				return;
 			}
 		}
-		
+
 		System.out.println("Le numero de reservation n'existe pas");
 		System.out.println("Voulez vous reessayer de supprimer une reservation? (OUI/NON)");
 		String decision = scanner.nextLine();
-		
-		if(decision.equals("OUI")) {
+
+		if (decision.equals("OUI")) {
 			deleteReservation();
 		}
 		return;
@@ -76,22 +80,23 @@ public class ReservationController {
 
 	public void confirmReservation() {
 		Scanner scanner = new Scanner(System.in);
-		
+
 		System.out.println("\nEntrer le numero de reservation de la reservation a confirmer (6 chiffres) : ");
 		String temp = scanner.nextLine();
 		int number = 0;
-		
+
 		try {
 			number = Integer.parseInt(temp);
-		}catch (NumberFormatException e){
+		} catch (NumberFormatException e) {
 			System.out.println("Numero de reservation invalide");
 			return;
 		}
-		
-		for(Reservation reservation : VaxTodo.reservations) {
-			if(reservation.getReservationNumber() == number) {
+
+		for (Reservation reservation : VaxTodo.reservations) {
+			if (reservation.getReservationNumber() == number) {
 				System.out.println("Reservation de " + reservation.getFirstName() + " " + reservation.getLastName());
-				System.out.println("Dose #"+ reservation.getDoseNumber() + " qui a lieu le " + reservation.getVisitDate() + " a " + reservation.getVisitTime());
+				System.out.println("Dose #" + reservation.getDoseNumber() + " qui a lieu le "
+						+ reservation.getVisitDate() + " a " + reservation.getVisitTime());
 				return;
 			}
 		}
@@ -100,21 +105,37 @@ public class ReservationController {
 	}
 
 	public void createReport() {
-		
+		User utilisateur;
+		System.out.println("\nCréation et Affichage des informations lié au rapport de vaccination");
+		try {
+			File myObj = new File("rapport.txt");
+			FileWriter myWriter = new FileWriter("rapport.txt");
+			if (myObj.createNewFile()) {
+				System.out.println("File created: " + myObj.getName());
+				myWriter.write("voici votre rapport de vaccination");
+				myWriter.close();
+			} else {
+				System.out.println("File already exists.");
+			}
+		} catch (IOException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
 	}
 
 	public void notifyVisitors() {
-		
+		System.out.println("votre rapport de vaccination a été envoye a votre adresse courriel");
+
 	}
-	
-	public void reservationList(){
+
+	public void reservationList() {
 		System.out.println("\nAffichage des informations des reservations");
 		PriorityQueue<Reservation> queue = new PriorityQueue<Reservation>(VaxTodo.reservations);
-		
-		while(!queue.isEmpty()){
+
+		while (!queue.isEmpty()) {
 			Reservation temp = queue.poll();
-			
-			System.out.println(temp.getFirstName() + " " + temp.getLastName() + ", le " + temp.getVisitDate() + " a " 
+
+			System.out.println(temp.getFirstName() + " " + temp.getLastName() + ", le " + temp.getVisitDate() + " a "
 					+ temp.getVisitTime() + " pour une " + String.valueOf(temp.getDoseNumber()) + "e dose");
 
 		}
